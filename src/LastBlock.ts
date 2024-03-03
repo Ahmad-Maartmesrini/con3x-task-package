cb9d3289f3f361cb62214c20da67560f10b4bc13
Ahmad Maartmesrini <Ahmad-Maartmesrini@outlook.com>
2024-03-03 21:02:55 +0300

    add getLastBlockNumber function

--- src/LastBlock.ts
+++ src/LastBlock.ts
@@ -1,6 +1,6 @@
 import { Network, Alchemy } from "alchemy-sdk";
 
-export default async function getLastBlockNumber(key: string) {
+export async function getLastBlockNumber(key: string) {
     const network = Network.ETH_MAINNET;
     const apiKey = key;
     const alchemy = new Alchemy({ network, apiKey });
